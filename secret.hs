577071c9ca2bad4e958ddb9f0e98d0422fc414dc
Andrey Feofilaktov <snusmumrik@snusmumrik4.haze.yandex.net>
2018-02-20 15:32:44 +0300

    Added explanations for secret, secret2

---
+++ secret.hs
@@ -0,0 +1,14 @@
+-- secret :: (Int -> a) -> a
+-- secret f = f <number>
+--
+-- where <number> -- any number in range of Int
+--
+--
+--
+-- secret2 :: (Int -> Bool) -> Bool
+-- secret2 f = True
+--
+-- secret2 :: (Int -> Bool) -> Bool
+-- secret2 f = False
+--
+-- Any of secret realizations
